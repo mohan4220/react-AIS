e7cb4490f8bcd92b4483a3122196ea01cf9164bb
mohan <mohan.krishna4220@gmail.com>
2020-05-06 16:30:21 +0530

    Updates

---
+++ precache-manifest.d4f2c077528a0dd9d8ccec4f08b4ec2f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a456480a03aff1eb08843fedb143f82",
+    "url": "/ais-visualizer/index.html"
+  },
+  {
+    "revision": "1f1a99946fda9b3daa79",
+    "url": "/ais-visualizer/static/css/main.f2e3b841.chunk.css"
+  },
+  {
+    "revision": "3e1079d8db41451a3ad9",
+    "url": "/ais-visualizer/static/js/2.8e52ec1c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ais-visualizer/static/js/2.8e52ec1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f1a99946fda9b3daa79",
+    "url": "/ais-visualizer/static/js/main.5c4a7d77.chunk.js"
+  },
+  {
+    "revision": "c7e84bb31cfb71a24d56",
+    "url": "/ais-visualizer/static/js/runtime-main.aad446a8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ais-visualizer/static/media/logo.5d5d9eef.svg"
+  }
+]);
